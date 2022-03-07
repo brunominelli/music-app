01189cea889ae5c81ed3d31910dd2b17bffa3c11
Bruno Minelli <minellibruno@hotmail.com>
2022-03-07 19:38:35 -0300

    Requisito 05 finalizado.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -8,6 +8,8 @@ class Search extends Component {
     this.state = {
       user: '',
       loading: true,
+      search: '',
+      isDisabled: true,
     };
   }
 
@@ -24,12 +26,53 @@ class Search extends Component {
     });
   }
 
+  handleSearch = ({ target: { value } }) => {
+    const min = 2;
+    if (value.length >= min) {
+      this.setState({
+        search: value,
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        search: value,
+        isDisabled: true,
+      });
+    }
+  }
+
+  handleSearchButton = (event) => {
+    event.preventDefault();
+    this.setState({
+      search: '',
+      isDisabled: true,
+    });
+  }
+
   render() {
-    const { user, loading } = this.state;
+    const { user, loading, search, isDisabled } = this.state;
     return (
       <>
         <Header user={ user } loading={ loading } />
-        <div data-testid="page-search" />
+        <div data-testid="page-search">
+          <form>
+            <input
+              type="text"
+              placeholder="Buscar..."
+              data-testid="search-artist-input"
+              value={ search }
+              onChange={ this.handleSearch }
+            />
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ isDisabled }
+              onClick={ this.handleSearchButton }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </>
     );
   }
