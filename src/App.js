341cff3453c1529160428cece40aafae5119bdc5
Bruno Minelli <minellibruno@hotmail.com>
2022-03-07 16:46:17 -0300

    Requisito 02 concluido.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,89 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Switch, Route } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
+import Loading from './pages/Loading';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      setSearch: false,
+      setRedirect: false,
+      user: '',
+    };
+  }
+
+  handleSearch = async () => {
+    const { user } = this.state;
+    await createUser({ name: user });
+    this.setState({
+      user: '',
+      setSearch: true,
+    });
+  }
+
+  handleLoginButton = (event) => {
+    event.preventDefault();
+    this.setState({
+      setSearch: false,
+      setRedirect: true,
+    }, this.handleSearch);
+  }
+
+  handleLogin = ({ target: { value } }) => {
+    const min = 3;
+    if (value.length >= min) {
+      this.setState({
+        isDisabled: false,
+        user: value,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+        user: value,
+      });
+    }
+  }
+
   render() {
+    const { isDisabled, setSearch, setRedirect, user } = this.state;
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/search" component={ Search } />
-          <Route exact path="/" component={ Login } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+      <>
+        <p>Trybetunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/search">
+              {setSearch
+                ? <Search />
+                : <Loading />}
+            </Route>
+            <Route exact path="/">
+              {setRedirect
+                ? <Redirect to="/search" />
+                : (
+                  <Login
+                    isDisabled={ isDisabled }
+                    handleLogin={ this.handleLogin }
+                    handleLoginButton={ this.handleLoginButton }
+                    user={ user }
+                  />)}
+            </Route>
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   render() {
+    const { isDisabled, handleLogin, handleLoginButton } = this.props;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <input
+            type="text"
+            placeholder="Nome"
+            data-testid="login-name-input"
+            onChange={ handleLogin }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isDisabled }
+            onClick={ handleLoginButton }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  isDisabled: PropTypes.bool.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+  handleLoginButton: PropTypes.func.isRequired,
+};
 
 export default Login;
