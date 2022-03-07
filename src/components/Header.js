d49971e1118bcdd90c2275969041bff47223153b
Bruno Minelli <minellibruno@hotmail.com>
2022-03-07 18:27:44 -0300

    Requisito 04 finalizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 
 class Header extends Component {
@@ -7,11 +8,16 @@ class Header extends Component {
     const { user, loading } = this.props;
     return (
       <header data-testid="header-component">
-        {
-          loading
-            ? <Loading />
-            : <p data-testid="header-user-name">{ user }</p>
-        }
+        {loading
+          ? <Loading />
+          : (
+            <nav>
+              <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+              <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+              <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+              <p data-testid="header-user-name">{ user }</p>
+            </nav>
+          )}
       </header>
     );
   }
